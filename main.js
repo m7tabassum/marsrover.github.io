93f96a3f8ca0a6dc92552c6946d2ba9edc91bd14
m7tabassum <91295405+m7tabassum@users.noreply.github.com>
2021-10-21 17:54:27 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+rover_width=100;
+rover_height=90;
+rover_x=10;
+rover_y=10;
+background_image="mars.jpg";
+rover_image="rover.png";
+function add(){
+    background_imgTag=new Image();
+    background_imgTag.onload=uploadbackground;
+    background_imgTag.src=background_image;
+    rover_imgTag=new Image();
+    rover_imgTag.onload=uploadrover;
+    rover_imgTag.src=rover_image;
+}
+function uploadbackground(){
+    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+} 
+function uploadrover(){
+    ctx.drawImage(rover_imgTag,rover_x,rover_y,rover.width,rover.height);
+}
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keyPressed=e.keyCode;
+    if(keyPressed=="38"){up(); }
+    if(keyPressed=="40"){down(); }
+    if(keyPressed=="37"){left(); }
+    if(keyPressed=="39"){right(); }
+}
